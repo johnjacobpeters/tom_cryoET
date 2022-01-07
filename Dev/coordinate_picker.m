0ce2c4c627ebaf72781d2a2fe72daf0986be11da
johnjacobpeters <johnjacobpeters@gmail.com>
2022-01-07 09:09:30 -0800

    Update coordinate_picker.m

--- Dev/coordinate_picker.m
+++ Dev/coordinate_picker.m
@@ -7,8 +7,3 @@ for i = 1:length(ls(*filt.mrc))
 end
 %[status,cmdout] = system([ChimeraX_dir '/ChimeraX ' Input_ChimeraX_command_file]);
 
-
-open /Users/johnpeters/Desktop/Lab/tom_cryoET-main/Extract/extract_tomo/201810XX_MPI/SV5_001_dff/SV5_001_dff000006a_filt.mrc;
-set bgColor white;
-volume #1 level 0.7;
-color radial #1.1 palette #ff0000:#ff7f7f:#ffffff:#7f7fff:#0000ff center 127.5,127.5,127.5
